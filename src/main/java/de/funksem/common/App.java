7b365818e370ccc7f35e3b7056c36c2c290bdb41
tom_h_ <holzhaus16@googlemail.com>
2014-08-06 14:20:13 +0200

    Test commit

--- src/main/java/de/funksem/common/App.java
+++ src/main/java/de/funksem/common/App.java
@@ -2,12 +2,13 @@ package de.funksem.common;
 
 /**
  * Hello world!
- * 
+ *
  */
 public class App
 {
     public static void main(String[] args)
     {
+        System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
---
+++ src/main/java/de/funksem/common/ClassA.java
@@ -0,0 +1,11 @@
+package de.funksem.common;
+
+public class ClassA
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
